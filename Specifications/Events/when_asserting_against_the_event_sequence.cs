c0f935f8f8d7c95b318fa5ce1f956d4414a62c7b
Michael Smith <michael@dolittle.com>
2019-02-11 15:37:59 +0100

    Unsuccessful asserts against the event sequence should generate SpecificationExceptions

--- Specifications/Events/when_asserting_against_the_event_sequence.cs
+++ Specifications/Events/when_asserting_against_the_event_sequence.cs
@@ -2,18 +2,15 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  * --------------------------------------------------------------------------------------------*/
+using System;
 using Machine.Specifications;
 
 namespace Dolittle.Machine.Specifications.Events
 {
 
     [Subject("Asserting against the Uncommitted Event Stream")]
-    public class when_asserting_against_the_event_sequence : an_aggregate_root_with_uncommitted_events
+    public class when_asserting_succesfully_against_the_event_sequence : an_aggregate_root_with_uncommitted_events
     {
-        static object an_event_in_first_position;
-        static object another_event_in_last_position;
-
-
         It should_have_an_event_at_the_beginning = () => aggregate_root.ShouldHaveEvent<AnEvent>().AtBeginning();
         It should_have_another_event_at_the_end = () => aggregate_root.ShouldHaveEvent<AnotherEvent>().AtEnd();
         It should_have_an_event_at_sequence_number_1 = () => aggregate_root.ShouldHaveEvent<AnEvent>().AtSequenceNumber(1);
